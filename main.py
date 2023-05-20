3f66b425e51d0ada0580fc4e914077c55fa32495
ejlacsamana <ejohnlacsamana@outlook.com>
2023-05-20 20:34:12 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# Scrape email for request from HR Business Partner
+# Download attached CSV
+# Clean CSV
+# Create promotion letters based on CSV content
+# Send promotion letters to employees using email addresses in CSV
